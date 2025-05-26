817966a189c333411fa397ff9f3b780cc0028384
Sergey Philippov <quadratte@gmail.com>
2025-05-26 16:33:55 +0700

    Initial Commit

---
+++ SB2.2Lesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SB2.2Lesson
+//
+//  Created by Quadratte on 26.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
